89ce30989079c0d5c5557c902f5302a1d959b3f8
chochey <30965960+chochey@users.noreply.github.com>
2023-08-31 23:23:22 -0400

    d

--- scripts.js
+++ scripts.js
@@ -40,3 +40,93 @@ function type() {
 window.onload = type;
 
 // ----------------------------------------------------------------
+
+// Get the modal
+var modal = document.getElementById("contactModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("contactButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// Set the action attribute for the contact form
+document.querySelector("#contactModal form").action =
+  "https://formspree.io/f/xnqkvnqo";
+
+// Get the modal
+var modal = document.getElementById("contactModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("contactButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// Set the action attribute for the contact form
+document.querySelector("#contactModal form").action =
+  "https://formspree.io/f/xnqkvnqo";
+
+// Get the modal
+var modal = document.getElementById("contactModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("contactButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// Set the action attribute for the contact form
+document.querySelector("#contactModal form").action =
+  "https://formspree.io/f/xnqkvnqo";
